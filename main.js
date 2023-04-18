8e89f777d4983e5eaebed0f9da59b22ccc9b6f83
eman1ashraf <129719834+eman1ashraf@users.noreply.github.com>
2023-04-18 06:00:27 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+
+var darkMode = false;
+if (window.matchMedia('(prefers-color-scheme: dark)').matches){
+darkMode = true;
+
+}
+if (localStorage.getItem('theme')=='dark'){
+    darkMode =true;
+}
+else if (localStorage.getItem('theme')=='light'){
+
+    darkMode= false;
+}
+if (darkMode){
+    document.body.classList.toggle('dark');
+}
+document.addEventListener('DOMContentLoaded',()=>{
+    document.getElementById('theme-toggle').addEventListener('click',()=>{
+        document.body.classList.toggle('dark');
+        localStorage.setItem('theme',document.body.classList.contains('dark')?
+        'dark':'light');
+    });
+});
